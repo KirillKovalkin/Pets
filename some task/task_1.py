abe7352ee8a127896d94f368d856117fbd9a6a93
Kirill Kovalkin <108697657+KirillKovalkin@users.noreply.github.com>
2024-12-25 05:08:03 +0300

    Update task_1.py

--- some task/task_1.py
+++ some task/task_1.py
@@ -3,6 +3,6 @@
 # Необходимо создать новый список, содержащий модели бренда Apple.
 
 mobiles = ["Xiaomi Redmi Note 10S", "Смартфон Xiaomi Redmi Note 10 Pro", "Apple iPhone 13", "Apple iPhone 11", "Huawei nova Y70", "Смартфон Apple iPhone 13 Pro"]
-apple = [mobiles for mobiles in mobiles if "Apple" in mobiles]
+apple = [mobile for mobile in mobiles if "Apple" in mobile]
 
 print(apple)
