5188a9f80c702581fe81a76376694e6a9982c5c1
Kirill Kovalkin <108697657+KirillKovalkin@users.noreply.github.com>
2025-05-26 10:08:38 +0300

    Update task_2.py

--- some task/task_2.py
+++ some task/task_2.py
@@ -3,54 +3,26 @@
 # Задать три экземпляра класса Cow, Cat, Dog с уникальными атрибутами для каждого.
 # Для каждого экземпляра вывести строку вида «<Name> is <Color>. It says <Voice>.»
 
-# class Animal():
-#     """Описание класса Animal"""
-#     def __init__(self, name, color, voice):
-#         """Свойства класса Animal"""
-#         self.name = name
-#         self.color = color
-#         self.voice = voice
-#
-#     def animal_properties(self):
-#         """Метод вывыодит свойства животного"""
-#         print('"' + self.name + " is " + self.color + ". It's says " + self.voice + '"')
-#
-# Animal1 = Animal('Cow', 'Brown', 'Moooo')
-# Animal2 = Animal('Cat', 'White', 'Meoow')
-# Animal3 = Animal('Dog', 'Black', 'Woof-Woof')
-#
-#
-#
-# Animal1.animal_properties()
-# Animal2.animal_properties()
-# Animal3.animal_properties()
 
-class Animal():
+class Animal:
     """Описание класса Animal"""
+
     def __init__(self, name, color, voice):
         """Свойства класса Animal"""
         self.name = name
         self.color = color
         self.voice = voice
 
-        if name == "Dog":
-            self.voice = " It's says Woof-Woof"
-        elif name == "Cat":
-            self.voice = " It's says Meoow"
-        elif name == "Cow":
-            self.voice = " It's says Moooo"
-        else:
-            self.voice = " I don't know the voice of this animal"
-
     def animal_properties(self):
-        """"Метод вывыодит свойства животного"""
-        print('"' + self.name + " is " + self.color + "." + self.voice + '"')
+        """Метод вывыодит свойства животного"""
+        return f'"{self.name} is {self.color}. It says {self.voice}"'
 
 
-Animal1 = Animal('Cow', 'Brown', 'Moooo')
-Animal2 = Animal('Cat', 'White', 'Meoow')
-Animal3 = Animal('Dog', 'Black', 'Woof-Woof')
+cow = Animal("Cow", "Brown", "Moooo")
+cat = Animal("Cat", "White", "Meoow")
+dog = Animal("Dog", "Black", "Woof-Woof")
 
-Animal1.animal_properties()
-Animal2.animal_properties()
-Animal3.animal_properties()
+
+print(cow.animal_properties())
+print(cat.animal_properties())
+print(dog.animal_properties())
